d19ff2c8be3e7d4f426eb8a304889a4136e4bdab
Vitalik0001 <vitalik.n1803@gmail.com>
2025-08-18 11:16:14 +0300

    fix: small fixes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import 'fabric';
+
 import './src/components/image-annotation/image-annotation.js';
 import './src/components/annotations-editor/annotations-editor.js';
 import './src/components/annotations-viewer/annotations-viewer.js';
